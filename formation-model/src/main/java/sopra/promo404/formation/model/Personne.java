ce89cff7eb88ddf658b0098f2b3354d9af66d9e9
Systank <lucas.crappeel@laposte.net>
2018-08-10 10:47:30 +0200

    Mapping de la classe Personne v1

--- formation-model/src/main/java/sopra/promo404/formation/model/Personne.java
+++ formation-model/src/main/java/sopra/promo404/formation/model/Personne.java
@@ -1,9 +1,27 @@
 package sopra.promo404.formation.model;
 
+import javax.persistence.Column;
+import javax.persistence.DiscriminatorColumn;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+import javax.persistence.Table;
+
+@Entity
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name = "disc")
+@Table(name = "person")
 public abstract class Personne {
+	@Id
+	@GeneratedValue
 	private Long id;
+	@Column(name="last_name")
 	private String nom;
+	@Column(name="first_name")
 	private String prenom;
+	//Embedable ?
 	private Adresse adresse;
 
 	public Personne() {

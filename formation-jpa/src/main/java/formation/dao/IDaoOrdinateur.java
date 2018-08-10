414223494104da051a4ecd96398f39c2ccff1b8d
Systank <lucas.crappeel@laposte.net>
2018-08-10 16:01:47 +0200

    Ajout des Dao Ordinateur

---
+++ formation-jpa/src/main/java/formation/dao/IDaoOrdinateur.java
@@ -0,0 +1,7 @@
+package formation.dao;
+
+import sopra.promo404.formation.model.Ordinateur;
+
+public interface IDaoOrdinateur extends IDao<Ordinateur, String>{
+
+}
---
+++ formation-jpa/src/main/java/formation/dao/jpa/DaoOrdinateurJPA.java
@@ -0,0 +1,5 @@
+package formation.dao.jpa;
+
+public class DaoOrdinateurJPA {
+
+}

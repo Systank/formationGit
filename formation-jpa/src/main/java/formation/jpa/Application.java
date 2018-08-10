49d0d65ddc5a641cef281be7abc2f13282c5ba9b
Systank <lucas.crappeel@laposte.net>
2018-08-10 14:37:42 +0200

    Création du SingleTon

---
+++ formation-jpa/src/main/java/formation/jpa/Application.java
@@ -0,0 +1,33 @@
+package formation.jpa;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Application {
+	private static Application instance = null;
+
+	private String jdbcUrl = "jdbc:postgresql://localhost:5432/formation-jpa";
+	private String jdbcUser = "postgres";
+	private String jdbcPassword = "admin";
+
+	private Application() {
+		try {
+			Class.forName("org.postgresql.Driver");
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static Application getInstance() {
+		if (instance == null) {
+			instance = new Application();
+		}
+
+		return instance;
+	}
+
+	public Connection createConnection() throws SQLException {
+		return DriverManager.getConnection(jdbcUrl, jdbcUser, jdbcPassword);
+	}
+}
